7494b978da3c3f8d3153e5ee0e0509953e912c67
dpsk331 <dpsk331@naver.com>
2021-04-03 01:14:58 +0900

    Lombok 설정 전

--- src/main/java/com/example/study/model/SearchParam.java
+++ src/main/java/com/example/study/model/SearchParam.java
@@ -1,12 +1,24 @@
 package com.example.study.model;
 
+// 스프링부트에서 jackson 라이브러리를 통해 JSON 형태인 {"account" : "","email" : "","page" :0} 와 같이 변경하려 반환!
+
 public class SearchParam {
 
     private String account;
     private String email;
     private int page;
 
-    // 스프링부트에서 jackson 라이브러리를 통해 JSON 형태인 {"account" : "","email" : "","page" :0} 와 같이 변경하려 반환!
+   public SearchParam() {}
+
+   public SearchParam(String account) {
+       this.account = account;
+   }
+
+   public SearchParam(String account, String email, int page) {
+       this.account = account;
+       this.email = email;
+       this.page = page;
+   }
 
     public String getAccount() {
         return account;
